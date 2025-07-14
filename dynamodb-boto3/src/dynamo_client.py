762cc33131f9679e8552aec9f69bbd6cfcbebfcf
priscilla clark <priscillaclark@gmail.com>
2025-07-14 00:54:35 -0300

    new file:   src/dynamo_client.py

---
+++ dynamodb-boto3/src/dynamo_client.py
@@ -0,0 +1,10 @@
+import boto3
+
+def get_dynamo_client():
+    return boto3.client(
+        'dynamodb',
+        endpoint_url='http://localhost:4566',
+        region_name='us-east-1',
+        aws_access_key_id='test',
+        aws_secret_access_key='test'
+    )
